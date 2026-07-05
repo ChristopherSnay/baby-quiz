18644f7ccfefa361d6950875e27b2d9be5b24d77
Christopher Snay <christopher.snay@gmail.com>
2026-07-05 01:21:03 -0400

    Adds colors mode

---
+++ src/constants/colors.const.tsx
@@ -0,0 +1,96 @@
+import {
+  blue,
+  brown,
+  green,
+  orange,
+  pink,
+  purple,
+  red,
+  yellow
+} from '@mui/material/colors';
+export const colors = [
+  {
+    id: 1,
+    name: 'Red',
+    icon: (
+      <div className="w-100" style={{ backgroundColor: red[700], height: '100px' }}></div>
+    )
+  },
+  {
+    id: 2,
+    name: 'Orange',
+    icon: (
+      <div
+        className="w-100"
+        style={{ backgroundColor: orange[700], height: '100px' }}
+      ></div>
+    )
+  },
+  {
+    id: 3,
+    name: 'Yellow',
+    icon: (
+      <div
+        className="w-100"
+        style={{ backgroundColor: yellow[600], height: '100px' }}
+      ></div>
+    )
+  },
+  {
+    id: 4,
+    name: 'Green',
+    icon: (
+      <div
+        className="w-100"
+        style={{ backgroundColor: green[700], height: '100px' }}
+      ></div>
+    )
+  },
+  {
+    id: 5,
+    name: 'Blue',
+    icon: (
+      <div
+        className="w-100"
+        style={{ backgroundColor: blue[900], height: '100px' }}
+      ></div>
+    )
+  },
+  {
+    id: 6,
+    name: 'Purple',
+    icon: (
+      <div
+        className="w-100"
+        style={{ backgroundColor: purple[800], height: '100px' }}
+      ></div>
+    )
+  },
+  {
+    id: 7,
+    name: 'Pink',
+    icon: (
+      <div
+        className="w-100"
+        style={{ backgroundColor: pink[200], height: '100px' }}
+      ></div>
+    )
+  },
+  {
+    id: 8,
+    name: 'Brown',
+    icon: (
+      <div
+        className="w-100"
+        style={{ backgroundColor: brown[500], height: '100px' }}
+      ></div>
+    )
+  },
+  {
+    id: 9,
+    name: 'Black',
+    icon: (
+      <div className="w-100" style={{ backgroundColor: 'black', height: '100px' }}></div>
+    )
+  }
+];
--- src/hooks/gridItems.tsx
+++ src/hooks/gridItems.tsx
@@ -1,4 +1,5 @@
 import { useMemo, useState } from 'react';
+import { colors } from '../constants/colors.const';
 import { vehicles } from '../constants/vehicles.const';
 import type { GridItem } from '../models/GridItem';
 import { shuffle } from '../utils/shuffle';
@@ -7,12 +8,13 @@ const useGridItems = (mode: string) => {
   const [items, setItems] = useState<GridItem[]>([]);
 
   useMemo(() => {
-    switch (mode) {
-      case 'Trucks':
+    switch (mode.toLocaleLowerCase()) {
+      case 'trucks':
         setItems(shuffle(vehicles));
         return;
-      case 'Shapes':
-        return [];
+      case 'colors':
+        setItems(shuffle(colors));
+        return;
       default:
         return [];
     }
